3f65bd47eb45f8899001122f389fb7dbc4310c0b
Elena Demidionok <vhelen0404@gmail.com>
2022-04-03 23:46:32 -0600

    axios call to retrieve data

---
+++ src/utils/API.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+const showSearchResults = (searchParam, setData) => {
+    const URL = 'https://i1cqoys68c-dsn.algolia.net/1/indexes/stg_choicemarket_products/query?x-algolia-application-id=I1CQOYS68C&x-algolia-api-key=eac7b807c0109771a245855c7501fca3'
+    const data = `{"params":"query=${searchParam}&hitsPerPage=122121212121"}`
+    const headers = {
+        headers: {
+            Accept: 'application/json',
+            'content-type': 'application/x-www-form-urlencoded',
+        }
+    }
+    axios
+        .post(URL, data, headers)
+        .then(response => {
+            if (!response.data.hits.length) {
+                alert("Not a valid search parameter!")
+                return 
+            } 
+            console.log(response)
+            setData(response.data)
+        })
+        .catch(error => {
+            console.log(error)
+            alert(error)
+            return
+        })
+}
+
+export default showSearchResults
